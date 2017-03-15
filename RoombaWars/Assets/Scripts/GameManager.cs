364647b709228a9c3a0a875d3db911e813151bcf
Quang Nguyen <quangn94@gmail.com>
2017-03-15 13:48:45 +0000

    Adds StartMenu and passing variables between scenes

---
+++ RoombaWars/Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	private int numberOfPlayers;
+
+	// Use this for initialization
+	void Start () {
+		if(Scenes.getParameter ("numberOfPlayers") != "")
+			numberOfPlayers = int.Parse (Scenes.getParameter ("numberOfPlayers"));
+		print ("Number of players: " + numberOfPlayers);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ RoombaWars/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ToMainSceneWith2Players() {
+		// How to use in next Scene
+		// int numPlayers = (int) Scenes.getParam("numberOfPlayers");
+		Scenes.Load ("Asgeir", "numberOfPlayers", "2");
+	}
+
+	public void ToMainSceneWith3Players() {
+		Scenes.Load ("Asgeir", "numberOfPlayers", "3");
+	}
+
+	public void ToMainSceneWith4Players() {
+		Scenes.Load ("Asgeir", "numberOfPlayers", "4");
+	}
+}
