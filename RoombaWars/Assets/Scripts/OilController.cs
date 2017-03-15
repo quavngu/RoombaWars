4d1f73547feb1fba8c167cb04bb1c5a997ae6226
Ásgeir Frímannsson <asgeirfri@gmail.com>
2017-03-15 14:55:16 +0000

    fire go less boom for you cpmputer

--- RoombaWars/Assets/Scripts/OilController.cs
+++ RoombaWars/Assets/Scripts/OilController.cs
@@ -19,10 +19,13 @@ public class OilController : MonoBehaviour {
 	void OnTriggerStay(Collider other) {
 		if (other.gameObject.CompareTag ("Roomba")) {
 			float rotateDeg = Random.Range (-slipperyness, slipperyness);
-			other.GetComponentInChildren<Canvas>().gameObject.transform.Rotate (0f, 0f, 50f * rotateDeg * Time.deltaTime);
+			other.GetComponentInChildren<Canvas> ().gameObject.transform.Rotate (0f, 0f, 50f * rotateDeg * Time.deltaTime);
 			other.gameObject.transform.Rotate (0f, 50f * rotateDeg * Time.deltaTime, 0f);
 		}
-		else if (other.gameObject.CompareTag ("Fire") || other.gameObject.CompareTag ("Explosion")) {
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("Fire") || other.gameObject.CompareTag ("Explosion")) {
 			StartCoroutine (Ignite ());
 		}
 	}
