dffee6a5a09aa55934e73967033e93930b5f7483
Ásgeir Frímannsson <asgeirfri@gmail.com>
2017-03-21 22:32:51 +0000

    merge

---
+++ RoombaWars/Assets/BoardManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// For this to work you have to turn off timetraveler
+public class BoardManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Time.timeScale = 0;
+		StartCoroutine (Intro ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator Intro() {
+		// Show ready to roomble
+
+		yield return new WaitForSecondsRealtime(2f);
+		Time.timeScale = 1;
+	}
+}
---
+++ RoombaWars/Assets/ControllsController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllsController : MonoBehaviour {
+	
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public KeyCode[] GetControlls(int player) {
+		KeyCode[] controlls = new KeyCode[3];
+		if (player == 1) {
+			controlls [0] = KeyCode.A;
+			controlls [1] = KeyCode.W;
+			controlls [2] = KeyCode.D;
+		}
+		if (player == 2) {
+			controlls [0] = KeyCode.LeftArrow;
+			controlls [1] = KeyCode.UpArrow;
+			controlls [2] = KeyCode.RightArrow;
+		}
+		return controlls;
+	}
+}
