447f273ba0d6caa81ca802fed3b59315a1e4f49c
skuli <skuliarnas@gmail.com>
2017-03-22 13:59:58 +0000

    can see what pickup you have

--- RoombaWars/Assets/PickUpController.cs
+++ RoombaWars/Assets/PickUpController.cs
@@ -1,13 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class PickUpController : MonoBehaviour {
 	public GameObject powerUp;
 
+	private PlayerMovement parent;
+	private Text powerUpText;
+
 	// Use this for initialization
 	void Start () {
-		
+		parent = gameObject.GetComponentInParent<PlayerMovement> ();
+		if (parent.playerNum == 1) {
+			GameObject temp = GameObject.Find ("Player1PowerUp");
+			powerUpText = temp.GetComponent<Text> ();
+			powerUpText.text = "Player 1 power: ";
+		}else if (parent.playerNum == 2) {
+			GameObject temp = GameObject.Find ("Player2PowerUp");
+			powerUpText = temp.GetComponent<Text> ();
+			powerUpText.text = "Player 2 power: ";
+		}else if (parent.playerNum == 3) {
+			GameObject temp = GameObject.Find ("Player3PowerUp");
+			powerUpText = temp.GetComponent<Text> ();
+			powerUpText.text = "Player 3 power: ";
+		}else if (parent.playerNum == 4) {
+			GameObject temp = GameObject.Find ("Player4PowerUp");
+			powerUpText = temp.GetComponent<Text> ();
+			powerUpText.text = "Player 4 power: ";
+		}
 	}
 	
 	// Update is called once per frame
@@ -19,6 +40,7 @@ public class PickUpController : MonoBehaviour {
 		print (other.gameObject.tag);
 		if (other.gameObject.CompareTag ("PowerUp")) {
 			powerUp = other.gameObject.GetComponent<PowerUpController> ().powerUp;
+			powerUpText.text = other.name.Substring (0, other.name.Length - 7);
 			Destroy (other.gameObject);
 		}
 	}
